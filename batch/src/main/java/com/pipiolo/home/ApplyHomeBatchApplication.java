eab177638dac5eb6458376131bee02c5e55f55d3
Pipi-Olo <Copy.H13@gmail.com>
2021-11-24 01:32:04 +0900

    Update #4 - Batch Application
    패키지 변화

--- batch/src/main/java/com/pipiolo/home/ApplyHomeBatchApplication.java
+++ batch/src/main/java/com/pipiolo/home/ApplyHomeBatchApplication.java
@@ -1,11 +1,11 @@
-package com.pipiolo.home.batch;
+package com.pipiolo.home;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableBatchProcessing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.pipiolo.home")
 public class ApplyHomeBatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApplyHomeBatchApplication.class, args);
