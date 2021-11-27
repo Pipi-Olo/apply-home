6697d6fee4c334c0083fa1f17632716464380bf8
Pipi-Olo <Copy.H13@gmail.com>
2021-11-28 02:27:32 +0900

    Add #12 - Repository Test in Core Module

---
+++ core/src/test/java/com/pipiolo/home/ApplicationTest.java
@@ -0,0 +1,8 @@
+package com.pipiolo.home;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTest {
+    public void contextLoads() {}
+}
