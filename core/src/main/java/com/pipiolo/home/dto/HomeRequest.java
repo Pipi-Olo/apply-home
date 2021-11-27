4ca7f6572070adbae92d3767b29968224d598d2b
Pipi-Olo <Copy.H13@gmail.com>
2021-11-28 02:27:17 +0900

    Refactoring #12 - core, api modules

--- core/src/main/java/com/pipiolo/home/dto/HomeRequest.java
+++ core/src/main/java/com/pipiolo/home/dto/HomeRequest.java
@@ -2,6 +2,7 @@ package com.pipiolo.home.dto;
 
 import com.pipiolo.home.constant.HouseType;
 import com.pipiolo.home.constant.SubscriptionType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @ToString
+@Builder
 public class HomeRequest {
 
     private Long noticeId;
