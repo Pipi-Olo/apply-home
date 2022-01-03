feb327c75944f47cb0adb044307f664a3f70e764
Pipi-Olo <Copy.H13@gmail.com>
2022-01-03 13:16:22 +0900

    Add #31 - Exception

---
+++ api/src/main/java/com/pipiolo/home/controller/error/APIExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.pipiolo.home.controller.error;
+
+import com.pipiolo.home.constant.ErrorCode;
+import com.pipiolo.home.dto.ErrorResponse;
+import com.pipiolo.home.exception.GeneralException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.pipiolo.home.constant.ErrorCode.*;
+
+@RestControllerAdvice(annotations = {RestController.class})
+public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(GeneralException.class)
+    public ResponseEntity<ErrorResponse> general(GeneralException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        return new ResponseEntity<>(ErrorResponse.from(errorCode), errorCode.getHttpStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exception(Exception ex) {
+        return  new ResponseEntity<>(ErrorResponse.from(INTERNAL_ERROR), INTERNAL_ERROR.getHttpStatus());
+    }
+}
---
+++ api/src/main/java/com/pipiolo/home/exception/GeneralException.java
@@ -0,0 +1,17 @@
+package com.pipiolo.home.exception;
+
+import com.pipiolo.home.constant.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class GeneralException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final String message;
+
+    public GeneralException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+    }
+}
