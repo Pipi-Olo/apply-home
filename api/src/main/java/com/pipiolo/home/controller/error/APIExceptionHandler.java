e9945b32f866a33e573d9518a86f56751b277c82
Pipi-Olo <Copy.H13@gmail.com>
2022-01-04 17:36:55 +0900

    Refactoring #31 - ErrorCode, GeneralException
    기존 api 모듈에 있던 것을 core 모듈로 이동
    
    HttpStatus에 대한 정보가 필요해서 core 모듈에  web에 대한 의존성이 필요하나,
    이를 방지하고자 기존에는 api 모듈에 만들었음.
    
    허나, 이럴 경우 코어에서 api 모듈만 에러처리를 할 수 있어 core모듈로 이동
    문제점은 core모듈은 web에 대한 의존성이 없어 HttpStatus에 맵핑 불가.
    
    이를 방지하고자 ExceptionMapper abstract class  추가
    해당 추상 클래스는 우리가 만든 ErrorCode를 바탕으로 HttpStatus에 대응함

--- api/src/main/java/com/pipiolo/home/controller/error/APIExceptionHandler.java
+++ api/src/main/java/com/pipiolo/home/controller/error/APIExceptionHandler.java
@@ -7,28 +7,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
 
-import static com.pipiolo.home.constant.ErrorCode.*;
+import static com.pipiolo.home.constant.ErrorCode.INTERNAL_ERROR;
+import static com.pipiolo.home.constant.ErrorCode.VALIDATION_ERROR;
+import static com.pipiolo.home.exception.ExceptionMapper.mapTo;
 
 @RestControllerAdvice(annotations = {RestController.class})
-public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+public class APIExceptionHandler {
 
     @ExceptionHandler(GeneralException.class)
     public ResponseEntity<ErrorResponse> general(GeneralException ex) {
         ErrorCode errorCode = ex.getErrorCode();
-        return new ResponseEntity<>(ErrorResponse.from(errorCode), errorCode.getHttpStatus());
+        return new ResponseEntity<>(ErrorResponse.from(errorCode), mapTo(errorCode));
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ErrorResponse> validation(ConstraintViolationException e) {
-        return  new ResponseEntity<>(ErrorResponse.from(VALIDATION_ERROR), VALIDATION_ERROR.getHttpStatus());
+    public ResponseEntity<ErrorResponse> validation(ConstraintViolationException ex) {
+        return  new ResponseEntity<>(ErrorResponse.from(VALIDATION_ERROR), mapTo(VALIDATION_ERROR));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> exception(Exception ex) {
-        return  new ResponseEntity<>(ErrorResponse.from(INTERNAL_ERROR), INTERNAL_ERROR.getHttpStatus());
+        return  new ResponseEntity<>(ErrorResponse.from(INTERNAL_ERROR), mapTo(INTERNAL_ERROR));
     }
 }
