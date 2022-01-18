5f7008db9e090b35f625a60ab054ea3271267a59
Pipi-Olo <Copy.H13@gmail.com>
2022-01-19 01:24:04 +0900

    Add Use View Index Page

--- api/src/main/java/com/pipiolo/home/controller/HomeController.java
+++ api/src/main/java/com/pipiolo/home/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
 
     private final HomeService homeService;
 
-    @GetMapping()
+    @GetMapping("/index")
     public ModelAndView homes() {
         Map<String, Object> map = new HashMap<>();
         List<HomeResponse> homeList = homeService.getHomes();
