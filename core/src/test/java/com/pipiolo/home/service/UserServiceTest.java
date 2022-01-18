a8b4ea75bd117c4b322beb11ff22feab52033bf8
Pipi-Olo <Copy.H13@gmail.com>
2022-01-19 01:24:58 +0900

    Refactoring - Tests

---
+++ core/src/test/java/com/pipiolo/home/service/UserServiceTest.java
@@ -0,0 +1,27 @@
+package com.pipiolo.home.service;
+
+import com.pipiolo.home.dto.UserRequest;
+import com.pipiolo.home.dto.UserResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceTest {
+
+    private final UserService userService;
+
+    public UserServiceTest(@Autowired UserService userService) {
+        this.userService = userService;
+    }
+
+    @Test
+    void test() {
+        UserRequest request = new UserRequest("test@test.com", Boolean.TRUE, null);
+        UserResponse response = userService.upsert(request);
+
+        Assertions.assertThat(response.email())
+                .isEqualTo(request.email());
+    }
+}
