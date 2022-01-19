ee140f4c0178f0582b8a97b38a2ed8d4f5be0f29
Pipi-Olo <Copy.H13@gmail.com>
2022-01-20 00:11:20 +0900

    Refactoring - ExceptionMapper

--- api/src/main/java/com/pipiolo/home/exception/ExceptionMapper.java
+++ api/src/main/java/com/pipiolo/home/exception/ExceptionMapper.java
@@ -5,20 +5,11 @@ import org.springframework.http.HttpStatus;
 
 public abstract class ExceptionMapper {
     public static HttpStatus mapTo(ErrorCode errorCode) {
-        switch (errorCode) {
-            case OK:
-                return HttpStatus.OK;
-            case BAD_REQUEST:
-            case SPRING_BAD_REQUEST:
-            case VALIDATION_ERROR:
-            case NOT_FOUND:
-                return HttpStatus.BAD_REQUEST;
-            case INTERNAL_ERROR:
-            case SPRING_INTERNAL_ERROR:
-            case DATA_ACCESS_ERROR:
-                return HttpStatus.INTERNAL_SERVER_ERROR;
-            default:
-                return HttpStatus.INTERNAL_SERVER_ERROR;
-        }
+        return switch (errorCode) {
+            case OK -> HttpStatus.OK;
+            case BAD_REQUEST, SPRING_BAD_REQUEST, VALIDATION_ERROR, NOT_FOUND -> HttpStatus.BAD_REQUEST;
+            case INTERNAL_ERROR, SPRING_INTERNAL_ERROR, DATA_ACCESS_ERROR -> HttpStatus.INTERNAL_SERVER_ERROR;
+            default -> HttpStatus.INTERNAL_SERVER_ERROR;
+        };
     }
 }
