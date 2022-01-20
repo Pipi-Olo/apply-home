3e6374f5d76a340b5861495689febde5aa5a37bf
Pipi-Olo <Copy.H13@gmail.com>
2022-01-21 00:46:49 +0900

    Update - CodeDeploy 자동화

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,37 +2,39 @@
 
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=apply-home
+MODULE_NAME=api
 
-echo "> Copy Build Files"
+echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/$MODULE_NAME/build/libs/*.jar $REPOSITORY/
 
-echo "> Check RUNNING APPLICATION PID"
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
-echo "> RUNNING APPLICATION PID : $CURRENT_PID"
+echo "> 현재 구동 중인 애플리케이션 PID : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
         echo "> There is no RUNNING APPLICATION"
 else
         echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
+        kill -15 "$CURRENT_PID"
         sleep 5
 fi
 
-echo "> NEW APPLICATION"
+echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR NAME : $JAR_NAME"
 
-echo "> Add authority $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x "$JAR_NAME"
 
-echo "> Start $JAR_NAME"
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-dev.properties \
-        $JAR_NAME > $REPOSITORY/$JAR_NAME 2>&1 &
+        -Dspring.config.location=classpath:/application-real.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.profiles.active=real \
+        "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
