24d5f355c633c174b39daf91dfb2420a52c906d5
Pipi-Olo <Copy.H13@gmail.com>
2022-01-26 17:53:22 +0900

    Update - DTO

--- core/src/main/java/com/pipiolo/home/dto/UserRequest.java
+++ core/src/main/java/com/pipiolo/home/dto/UserRequest.java
@@ -6,14 +6,30 @@ import java.util.Set;
 
 public record UserRequest(
         String email,
+        String password,
         Boolean subscribed,
         Set<String> regions
 ) {
     public User toEntity() {
         return User.builder()
                 .email(email)
+                .password(password)
                 .subscribed(subscribed)
                 .regions(regions)
                 .build();
+    }
+
+    public static UserRequest of(
+            String email,
+            String password,
+            Boolean subscribed,
+            Set<String> regions
+    ) {
+        return new UserRequest(
+                email,
+                password,
+                subscribed,
+                regions
+        );
     }
 }
