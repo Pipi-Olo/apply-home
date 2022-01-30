a8334d804fec018f31895a484cac356e8fa0e116
Pipi-Olo <Copy.H13@gmail.com>
2022-01-31 04:14:28 +0900

    Update - Constants

--- core/src/main/java/com/pipiolo/home/constant/SubscriptionType.java
+++ core/src/main/java/com/pipiolo/home/constant/SubscriptionType.java
@@ -1,6 +1,15 @@
 package com.pipiolo.home.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SubscriptionType {
-    PARCEL, /* 분양 */
-    RENT    /* 임대 */
+
+    PARCEL("분양"),
+    RENT("임대")
+    ;
+
+    private final String type;
 }
