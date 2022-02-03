48f94ea577c1658a9240239c0180d7b129e17c0b
Pipi-Olo <Copy.H13@gmail.com>
2022-02-03 15:37:29 +0900

    Delete - UserService

--- core/src/main/java/com/pipiolo/home/service/UserService.java
+++ core/src/main/java/com/pipiolo/home/service/UserService.java
@@ -6,7 +6,6 @@ import com.pipiolo.home.dto.UserResponse;
 import com.pipiolo.home.exception.GeneralException;
 import com.pipiolo.home.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -14,8 +13,9 @@ import java.util.List;
 import static com.pipiolo.home.constant.ErrorCode.BAD_REQUEST;
 import static com.pipiolo.home.constant.ErrorCode.NOT_FOUND;
 
+@Deprecated
 @RequiredArgsConstructor
-@Service
+//@Service
 public class UserService {
 
     private final UserRepository userRepository;
