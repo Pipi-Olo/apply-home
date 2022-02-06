d64af070cf45cc19121c2c9c4f3e50d273118619
Pipi-Olo <Copy.H13@gmail.com>
2022-02-07 05:15:40 +0900

    Fix - Spring Security to Static Resources
    "/scss/**", "/vendor/**"에 대한 접근이 보호되어 있어, 아이콘 작동 불가 오류 해결

--- api/src/main/java/com/pipiolo/home/config/WebSecurityConfig.java
+++ api/src/main/java/com/pipiolo/home/config/WebSecurityConfig.java
@@ -22,6 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        web.ignoring().antMatchers("/scss/**", "/vendor/**");
     }
 
     @Override
