7e8c57ce25b28bbb83d41ae682c4f3f90e1059ee
Pipi-Olo <Copy.H13@gmail.com>
2022-02-07 07:15:18 +0900

    Update - Script

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,7 @@ echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-rm $$REPOSITORY/*-plain.jar
+rm $REPOSITORY/*-plain.jar
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
