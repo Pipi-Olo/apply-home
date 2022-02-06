db80efdb5c990a79e1387630a1cc1113c0735d5c
Pipi-Olo <Copy.H13@gmail.com>
2022-02-07 06:07:36 +0900

    Update - Scripts

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,16 +5,16 @@
 
 function find_idle_profile()
 {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [ ${RESPONSE_CODE} -ge 400 ]
-  then
-    CURRNT_PROFILE=real2
-  else
-    CURRNT_PROFILE=$(curl -s http://localhost/profile)
-  fi
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=real2
+    else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
 
-    if [ ${CURRNT_PROFILE} == real1 ]
+    if [ ${CURRENT_PROFILE} == real1 ]
     then
       IDLE_PROFILE=real2
     else
@@ -24,16 +24,15 @@ function find_idle_profile()
     echo "${IDLE_PROFILE}"
 }
 
-# 쉬고 있는 profile 의 port 찾기
+# 쉬고 있는 profile의 port 찾기
 function find_idle_port()
 {
-  IDLE_PROFILE=$(find_idle_profile)
+    IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE} == real1 ]
-  then
-    echo "8081"
-  else
-    echo "8082"
-  fi
-
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
 }
