42ada6bdbcfc43894f008e0469eca99a23574d10
Pipi-Olo <Copy.H13@gmail.com>
2022-02-09 02:44:33 +0900

    Fix - Error

--- api/src/main/resources/static/js/home.js
+++ api/src/main/resources/static/js/home.js
@@ -1,12 +1,22 @@
 let page_links = document.querySelectorAll(".page-link");
 for (let i = 0; i < page_links.length; i++) {
     page_links[i].addEventListener("click", function () {
-        let href = "http://localhost:8080/index";
+        let href = window.location.href;
+        if (href.indexOf("/index") == -1) {
+            href += "index";
+        }
+
         let pageNo = this.getAttribute('data-dt-idx') - 1;
         if (pageNo < 0) {
             pageNo = 0;
         }
 
-        location.href = href + "?page=" + pageNo.toString();
+        if (href.indexOf('page=') == -1) {
+            location.href = href + "?page=" + pageNo.toString();
+        }
+        else {
+            let split = href.split("page=");
+            location.href = split[0] + "page=" + pageNo.toString();
+        }
     });
 }
