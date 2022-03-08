7f809ad271a2f62d2b495b9c7ac0a2ae4a9545ea
Pipi-Olo <Copy.H13@gmail.com>
2022-03-09 02:06:05 +0900

    Scripts Update

--- scripts/start.sh
+++ scripts/start.sh
@@ -34,8 +34,3 @@ cd $REPOSITORY/
 
 docker build -t spring ./
 docker run -it --name $IDLE_PROFILE -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT spring
-
-#nohup java -jar \
-#        -Dspring.config.location=classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-real-db.properties \
-#        -Dspring.profiles.active=$IDLE_PROFILE \
-#        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,16 +4,13 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-IDLE_PORT=$(find_idle_port)
 IDLE_PROFILE=$(find_idle_profile)
+
 CONTAINER_ID=$(docker container ls -f "name=${IDLE_PROFILE}" -q)
 
-echo "> ${CONTAINER_ID} 에서 구동중인 애플리케이션 확인"
+echo "> 구동중인 컨테이너 ID는 ${CONTAINER_ID}"
 
-echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
-
-if [ -z ${CONTAINER_ID} ] # IDLE_PID
+if [ -z ${CONTAINER_ID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
@@ -23,7 +20,5 @@ else
   echo "> docker rm ${IDLE_PROFILE}"
   sudo docker rm ${IDLE_PROFILE}
 
-  echo "> kill -15 $IDLE_PID"
-  kill -15 ${IDLE_PID}
   sleep 5
 fi
